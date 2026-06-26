12dd230a36324a922bd7817d319b4b19dfa7df44
mysi925 <m4yasi@gmail.com>
2026-06-26 02:27:22 -0700

    Update SeoHeading.tsx

--- src/sections/PageShell/components/SeoHeading.tsx
+++ src/sections/PageShell/components/SeoHeading.tsx
@@ -1,10 +1,24 @@
 export const SeoHeading = () => {
   return (
-    <h1 className="box-border caret-transparent text-[6.66667px] font-bold tracking-[0.875px] leading-[10px] outline-[3px] break-words relative text-center no-underline w-[calc(100%_+_0.875px)] mt-[23.3333px] font-stix_two_text md:text-[8.66667px] md:tracking-[1.3px] md:leading-[13px] md:w-[calc(100%_+_1.3px)] md:mt-[34.6667px]">
-      <span className="box-border caret-transparent block text-[6.66667px] tracking-[0.875px] leading-[10px] outline-[3px] break-words relative no-underline md:text-[8.66667px] md:tracking-[1.3px] md:leading-[13px]">
-      
-        <br className="box-border caret-transparent text-[6.66667px] tracking-[0.875px] leading-[10px] outline-[3px] break-words no-underline md:text-[8.66667px] md:tracking-[1.3px] md:leading-[13px]" />
-  
+    <h1
+      className="
+        box-border caret-transparent
+        text-[6.66667px] font-bold
+        tracking-[0.875px] leading-[10px]
+        outline-[3px] break-words relative text-center no-underline
+        w-[calc(100%_+_0.875px)]
+        mt-[23.3333px]
+        md:text-[8.66667px]
+        md:tracking-[1.3px]
+        md:leading-[13px]
+        md:w-[calc(100%_+_1.3px)]
+        md:mt-[34.6667px]
+        text-gray-400
+      "
+    >
+      <span className="block text-inherit">
+        {/* SEO TEXT GOES HERE */}
+        <br />
       </span>
     </h1>
   );
